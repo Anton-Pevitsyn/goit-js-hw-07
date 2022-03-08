e5dc756b913a16b257530071dfd2a16ba73b11d9
Anton-Pevitsyn <75798691+Anton-Pevitsyn@users.noreply.github.com>
2022-03-08 22:33:30 +0400

    joinGallary

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainet = document.querySelector('.gallery');
+
+function createGallery() {
+  const galleryMarkup = galleryItems
+    .map(({ preview, description }) => {
+      return `<li class="gallery__item">
+  <img src="${preview}" alt="${description}" class="gallery__image">
+</li>`;
+    })
+    .join('');
+  galleryContainet.insertAdjacentHTML('afterbegin', galleryMarkup);
+}
+createGallery();
+
+function receivingUrl(event) {
+  const galleryItem = galleryItems.find(
+    ({ description }) => event.target.getAttribute('alt') === description,
+  );
+
+  return galleryItem.original;
+}
+
+galleryContainet.addEventListener('click', receivingUrl);
